12a2897a9ca9b9fed101a730dff2f52d0ae7e8fa
the-user01 <sakibcom02@gmail.com>
2024-11-21 01:59:26 +0600

    updated codes

--- index.js
+++ index.js
@@ -78,6 +78,14 @@ async function run() {
             res.send(result);
         })
 
+        app.delete('/services/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+
+            const result = await serviceCollection.deleteOne(query)
+            res.send(result);
+        })
+
 
         // User Message Related Api
 
